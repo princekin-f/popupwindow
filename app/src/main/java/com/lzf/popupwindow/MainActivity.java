1f36366f97bcab956f580c5f473afcfdad4a40cf
princekin <13063218706@163.com>
2018-02-03 23:45:00 +0800

    Initial commit

---
+++ app/src/main/java/com/lzf/popupwindow/MainActivity.java
@@ -0,0 +1,165 @@
+package com.lzf.popupwindow;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Property;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * @author liuzhenfeng
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView iv_add;
+    private TextView tv_1, tv_2, tv_3, tv_4, tv_5;
+    private PopupWindow mPopupWindow;
+
+    private AnimUtil animUtil;
+    private float bgAlpha = 1f;
+    private boolean bright = false;
+
+    private static final long DURATION = 500;
+    private static final float START_ALPHA = 0.7f;
+    private static final float END_ALPHA = 1f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 实现透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+
+        mPopupWindow = new PopupWindow(this);
+        animUtil = new AnimUtil();
+
+        iv_add = findViewById(R.id.iv_add);
+        iv_add.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_add:
+                showPop();
+                toggleBright();
+                break;
+            case R.id.tv_1:
+                mPopupWindow.dismiss();
+                Toast.makeText(this, tv_1.getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_2:
+                mPopupWindow.dismiss();
+                Toast.makeText(this, tv_2.getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_3:
+                mPopupWindow.dismiss();
+                Toast.makeText(this, tv_3.getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_4:
+                mPopupWindow.dismiss();
+                Toast.makeText(this, tv_4.getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_5:
+                mPopupWindow.dismiss();
+                Toast.makeText(this, tv_5.getText(), Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showPop() {
+        // 设置布局文件
+        mPopupWindow.setContentView(LayoutInflater.from(this).inflate(R.layout.pop_add, null));
+        // 设置pop透明效果
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable(0x0000));
+        // 设置pop出入动画
+        mPopupWindow.setAnimationStyle(R.style.pop_add);
+        // 设置pop获取焦点，如果为false点击返回按钮会退出当前Activity，如果pop中有Editor的话，focusable必须要为true
+        mPopupWindow.setFocusable(true);
+        // 设置pop可点击，为false点击事件无效，默认为true
+        mPopupWindow.setTouchable(true);
+        // 设置点击pop外侧消失，默认为false；在focusable为true时点击外侧始终消失
+        mPopupWindow.setOutsideTouchable(true);
+        // 相对于 + 号正下面，同时可以设置偏移量
+        mPopupWindow.showAsDropDown(iv_add, -100, 0);
+        // 设置pop关闭监听，用于改变背景透明度
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                toggleBright();
+            }
+        });
+
+        tv_1 = mPopupWindow.getContentView().findViewById(R.id.tv_1);
+        tv_2 = mPopupWindow.getContentView().findViewById(R.id.tv_2);
+        tv_3 = mPopupWindow.getContentView().findViewById(R.id.tv_3);
+        tv_4 = mPopupWindow.getContentView().findViewById(R.id.tv_4);
+        tv_5 = mPopupWindow.getContentView().findViewById(R.id.tv_5);
+
+        tv_1.setOnClickListener(this);
+        tv_2.setOnClickListener(this);
+        tv_3.setOnClickListener(this);
+        tv_4.setOnClickListener(this);
+        tv_5.setOnClickListener(this);
+    }
+
+    private void toggleBright() {
+        // 三个参数分别为：起始值 结束值 时长，那么整个动画回调过来的值就是从0.5f--1f的
+        animUtil.setValueAnimator(START_ALPHA, END_ALPHA, DURATION);
+        animUtil.addUpdateListener(new AnimUtil.UpdateListener() {
+            @Override
+            public void progress(float progress) {
+                // 此处系统会根据上述三个值，计算每次回调的值是多少，我们根据这个值来改变透明度
+                bgAlpha = bright ? progress : (START_ALPHA + END_ALPHA - progress);
+                backgroundAlpha(bgAlpha);
+            }
+        });
+        animUtil.addEndListner(new AnimUtil.EndListener() {
+            @Override
+            public void endUpdate(Animator animator) {
+                // 在一次动画结束的时候，翻转状态
+                bright = !bright;
+            }
+        });
+        animUtil.startAnimator();
+    }
+
+    /**
+     * 此方法用于改变背景的透明度，从而达到“变暗”的效果
+     */
+    private void backgroundAlpha(float bgAlpha) {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        // 0.0-1.0
+        lp.alpha = bgAlpha;
+        getWindow().setAttributes(lp);
+        // everything behind this window will be dimmed.
+        // 此方法用来设置浮动层，防止部分手机变暗无效
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+    }
+
+}
+
